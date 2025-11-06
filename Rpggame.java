cae13d2b51974a68121cb6d8f1ce7add92f99170
xXminimannenXx <anton.finn@elev.ga.klaragymnasium.se>
2025-11-07 00:39:29 +0100

    spaccung

--- Rpggame.java
+++ Rpggame.java
@@ -5,8 +5,8 @@ public class Rpggame {
 
      public static void main (String[] args){
         boolean running = true;
-         CharacterCreator.userCharacterCreator();
-          enviromentCheck.changeEnv( "outside");
+        CharacterCreator.userCharacterCreator();
+        enviromentCheck.changeEnv( "outside");
         while(running == true){
        
        
