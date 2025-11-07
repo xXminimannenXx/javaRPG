8b016d8c6735fd0a807e240fcd7c2aa32bba20df
xXminimannenXx <anton.finn@elev.ga.klaragymnasium.se>
2025-11-07 13:04:09 +0100

    fixa bug

--- CharacterCreator.java
+++ CharacterCreator.java
@@ -58,6 +58,11 @@ public class CharacterCreator {
                     return;
                 } else{
                     System.out.println("You need atleast 1 health to continue");
+                     try{
+                    Thread.sleep(500);
+                    }catch(InterruptedException e){
+                     e.printStackTrace();
+                     }
                 }
                 
                 
