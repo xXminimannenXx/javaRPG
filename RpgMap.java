582785912174ea468e728937ab0297436138cafe
xXminimannenXx <anton.finn@elev.ga.klaragymnasium.se>
2025-11-29 14:44:39 +0100

    bytt plats på playermovmeent

--- RpgMap.java
+++ RpgMap.java
@@ -89,7 +89,7 @@ public class RpgMap {
             }   
 
 
-            int[] newPos = playerMove(playerX, playerY, scanner, maxX, maxY);
+            int[] newPos = PlayerMovment.playerMove(playerX, playerY, scanner, maxX, maxY);
             playerX = newPos[0];
             playerY = newPos[1];
 
@@ -160,36 +160,6 @@ public class RpgMap {
         }
 
     }
-        static int[] playerMove(int playerX, int playerY, Scanner scanner, int maxX, int maxY) {
-        String input = scanner.nextLine();
-
-
-        if (input.length() == 0) {
-         // bara Enter
-         return new int[]{playerX, playerY};
-         }
-
-        char userInput = Character.toLowerCase(input.charAt(0));
-
-        switch (userInput) {
-            case 'w':
-                if (playerY > 0) playerY--; 
-                break;
-            case 's':
-                if (playerY < maxY-1) playerY++; 
-                break;
-            case 'a':
-                if (playerX > 0) playerX--;
-                break;
-            case 'd':
-                if (playerX < maxX-1) playerX++;
-                break;
-            case 'e':
-                scanner.close();
-                System.exit(0);
-                break;
-        }
-        return new int[]{playerX, playerY};
-    }
+     
 }
 
